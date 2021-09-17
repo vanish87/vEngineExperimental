c4ea22808be39098b3d4486463d7c548dc7118d5
Yuan Li <vanish8.7@gmail.com>
2021-09-17 11:33:17 +0900

    init files

---
+++ header/vector.h
@@ -0,0 +1,21 @@
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#pragma once
+
+#include <array>
+
+namespace vEngine
+{
+	namespace Core
+	{
+		class Vector
+		{
+			std::array<int, 2> data_;
+		};
+
+	}
+}
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "vector.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World" ;
+    vEngine::Core::Vector vec2;
+
+    return 0;
+}
