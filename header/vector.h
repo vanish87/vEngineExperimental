88b3995738f76c42cc8c87640155449ae79b2341
Yuan Li <vanish8.7@gmail.com>
2021-10-04 12:18:25 +0900

    update vector code

--- header/vector.h
+++ header/vector.h
@@ -5,14 +5,33 @@
 #pragma once
 
 #include <array>
+#include <iostream>
 
 namespace vEngine
 {
 	namespace Core
 	{
+		template <typename T = float, int N = 4>
 		class Vector
 		{
-			std::array<int, 2> data_;
+		private:
+			typedef std::array<T, N> DataType;
+
+			DataType data_;
+
+		public:
+			constexpr Vector() noexcept : data_{0} {}
+			explicit constexpr Vector(const Vector &other) {} //copy constructor
+			explicit constexpr Vector(const T &other) {std::cout<<"call";}
+			constexpr Vector &operator=(const Vector &other) { return *this;} //assignment operator
+
+			virtual ~Vector() {}// virtual destructor
+
+			constexpr Vector(const Vector &&other) {} //move constructor
+			constexpr Vector &operator=(const Vector &&other) {} //move operator
+
+			//ambiguous/undefined actions
+			explicit constexpr Vector(const T *other) noexcept {} // constructor obj with pointer
 		};
 
 	}
--- source/main.cpp
+++ source/main.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 #include "vector.h"
 
+using namespace vEngine::Core;
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World" ;
-    vEngine::Core::Vector vec2;
+    Vector<int,2> vec2;
+    // Vector<int,2> vec21 = vec2;
+
+    // vec2 = vec21;
+
+    Vector<int,3> v3(5);
 
     return 0;
 }
