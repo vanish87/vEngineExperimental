e72b477789c65fbf891f2334e4cc7a302006648b
Yuan Li <vanish8.7@gmail.com>
2021-10-25 12:58:35 +0900

    add big 5 comment

--- header/vector.h
+++ header/vector.h
@@ -21,11 +21,37 @@ namespace vEngine
 
 		public:
 			constexpr Vector() noexcept : data_{0} {}
-			explicit constexpr Vector(const Vector &other) {} //copy constructor
-			explicit constexpr Vector(const T &other) {std::cout<<"call";}
-			constexpr Vector &operator=(const Vector &other) { return *this;} //assignment operator
 
-			virtual ~Vector() {}// virtual destructor
+			explicit constexpr Vector(const T &other)
+			//explicit one parameter constructor to avoid implicit conversion
+			{
+
+			}
+
+			constexpr Vector(const Vector &other) 
+			//big five - 3: copy constructor
+			//called by 
+			// 1. ClassA a = b;
+			// 2. Func(ClassA a) -> pass by value
+			// 3. return temp; -> return by value
+			{
+
+			}
+
+			constexpr Vector &operator=(const Vector &other) noexcept
+			//big five - 2: assignment operator
+			//with const formal paramter
+			//with constexpr TODO: to clarify usage of constexpr
+			{
+				//1.check self assignment
+				//2.allocate new object before-> to assure memory before delete existing one
+				 return *this;
+			} 
+			virtual ~Vector() 
+			// big five - 1: virtual destructor -> assure desctor of subclass called
+			{
+
+			}
 
 			constexpr Vector(const Vector &&other) {} //move constructor
 			constexpr Vector &operator=(const Vector &&other) {} //move operator
--- source/main.cpp
+++ source/main.cpp
@@ -8,11 +8,11 @@ int main(int argc, char* argv[])
 {
     std::cout << "Hello World" ;
     Vector<int,2> vec2;
-    // Vector<int,2> vec21 = vec2;
+    Vector<int,2> vec21;
 
-    // vec2 = vec21;
+    vec2 = vec21;
 
-    Vector<int,3> v3(5);
+    Vector<int,3> v3 = 5;
 
     return 0;
 }
