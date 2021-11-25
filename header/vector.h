72c607fe0bd281a5be24633ccc7723bf1fff2b45
Yuan Li <vanish8.7@gmail.com>
2021-11-25 11:05:21 +0900

    udpate big five comment

--- header/vector.h
+++ header/vector.h
@@ -20,43 +20,72 @@ namespace vEngine
 			DataType data_;
 
 		public:
-			constexpr Vector() noexcept : data_{0} {}
 
-			explicit constexpr Vector(const T &other)
-			//explicit one parameter constructor to avoid implicit conversion
+		public:
+			constexpr Vector() noexcept
+				: data_{0} // use init list {} to initialize data
 			{
-
+				std::cout << "ctor called" << std::endl;
 			}
 
-			constexpr Vector(const Vector &other) 
-			//big five - 3: copy constructor
-			//called by 
-			// 1. ClassA a = b;
-			// 2. Func(ClassA a) -> pass by value
-			// 3. return temp; -> return by value
+			explicit constexpr Vector(const T &other)
+			// explicit one parameter constructor to avoid implicit conversion
 			{
+			}
 
+			constexpr Vector(const Vector &other)
+			// big five - 3: copy constructor
+			// called by
+			//  1. ClassA a = b;
+			//  2. Func(ClassA a) -> pass by value
+			//  3. return temp; -> return by value
+			{
 			}
 
 			constexpr Vector &operator=(const Vector &other) noexcept
-			//big five - 2: assignment operator
-			//with const formal paramter
-			//with constexpr TODO: to clarify usage of constexpr
+			// big five - 2: assignment operator
+			// with const formal paramter
+			// with constexpr TODO: to clarify usage of constexpr
+			// constexpr can not pass clang compiling => don't need it
 			{
-				//1.check self assignment
-				//2.allocate new object before-> to assure memory before delete existing one
-				 return *this;
-			} 
-			virtual ~Vector() 
-			// big five - 1: virtual destructor -> assure desctor of subclass called
+				// 1.check self assignment
+				if (this != &other)
+				{
+					// 2.allocate new object before-> to assure memory before delete existing one
+				}
+				return *this;
+			}
+			virtual ~Vector()
+			// big five - 1: virtual destructor -> assure destructor of subclass called
 			{
-
 			}
 
-			constexpr Vector(const Vector &&other) {} //move constructor
-			constexpr Vector &operator=(const Vector &&other) {} //move operator
+			constexpr Vector(Vector &&other)
+				// big five - 4: move constructor
+				// const is not need: constexpr Vector(const Vector &&other)
+				// The move constructor:
+				// - Takes an r-value reference as a parameter.
+				// - Discards the object’s current state.
+				// - Transfers ownership of the r-value object into the receiver
+				// - Puts the r-value object into an 'empty' state.
+				// - subclass must explicitly std::move the base class
+				: data_{std::move(other.data_)}
+			{
+				std::cout << "move ctor called" << std::endl;
+			}
+			constexpr Vector &operator=(const Vector &&other)
+			// big five - 5 : move operator
+			// is used like unique_ptr
+			{
+				// 1.check self assignment
+				if (this != &other)
+				{
+					// 2.allocate new object before-> to assure memory before delete existing one
+				}
+				return *this;
+			}
 
-			//ambiguous/undefined actions
+			// ambiguous/undefined actions
 			explicit constexpr Vector(const T *other) noexcept {} // constructor obj with pointer
 		};
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,27 @@
 
 #include <iostream>
+#include <vector>
 #include "vector.h"
 
 using namespace vEngine::Core;
 
+Vector<int,2> NewVec()
+{
+    return Vector<int,2>();
+}
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World" ;
-    Vector<int,2> vec2;
-    Vector<int,2> vec21;
+    // std::cout << "Hello World" ;
+    // Vector<int,2> vec2;
+    // Vector<int,2> vec21;
 
-    vec2 = vec21;
+    // vec2 = vec21;
 
-    Vector<int,3> v3 = 5;
+    std::vector<Vector<int,2>> moveList{};
+    
+    moveList.push_back(Vector<int,2>());
+    moveList.push_back(Vector<int,2>());
 
     return 0;
 }
