5ebc8c7578659c02388afbca60f1706737cf43a8
Yuan Li <vanish8.7@gmail.com>
2021-12-11 00:35:53 +0900

    add doc comment example

---
+++ source/engine/core/src/class.cpp
@@ -0,0 +1,31 @@
+/// \file class.cpp
+/// \brief source file for class Ctest.
+/// 
+/// A detailed file description.
+///
+/// \author author_name
+/// \version version_number
+/// \date xxxx-xx-xxx
+
+#include "class_header.h"
+
+/// A detailed namespace description, it
+/// should be 2 lines at least.
+namespace MyNamespace
+{
+  
+   /// constructor detailed defintion, 
+   /// should be 2 lines
+   MyClass::MyClass()
+     :PublicVariable(10)
+   {
+
+   }
+
+   /// A detailed function description, it
+   /// should be 2 lines at least.
+   int MyClass::GetVariable(int p1, float p2)
+   {
+       return 1;
+   }
+}
