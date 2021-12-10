03ed1e9ca2a7947c186644ae97b9e8cb162293b2
Yuan Li <vanish8.7@gmail.com>
2021-12-10 14:50:19 +0900

    update find freetype cmake

--- cmake/Findfreetype.cmake
+++ cmake/Findfreetype.cmake
@@ -20,7 +20,9 @@ find_path(FREETYPE_INCLUDE_DIR freetype/freetype.h
     ${EXTERNAL_BUILD_DIR}
     
     PATH_SUFFIXES
-    ${EXTERNAL_BUILD_DIR}/include/freetype2
+    ${EXTERNAL_BUILD_DIR}
+    /include
+    /include/freetype2
     
     DOC "The directory where FREETYPE/Importer.hpp etc. resides")
 
