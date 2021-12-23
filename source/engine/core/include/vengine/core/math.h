3d21ff920e59775e0242bae0a9ef0f6660448fc8
Yuan Li <vanish8.7@gmail.com>
2021-12-23 12:07:01 +0900

    format comment

--- source/engine/core/include/vengine/core/math.h
+++ source/engine/core/include/vengine/core/math.h
@@ -9,47 +9,47 @@
 namespace vEngine
 {
     /// \brief Math Functions \n
-	/// Brief contine
-	///
-	/// This implementation of Math/Engine libraries basically follows
-	/// conventions of DirectX/HLSL/Windows.
-	/// 
-	/// Row major system
-	/// ========================
-	/// - Vector is row major [x,y,z,w]
-	/// - Matrix is row major \n
-	/// \code 
-	/// [e0, e1, e2, e3 ]
-	/// [e4, e5, e6, e7 ]
-	/// [e8, e9, e10,e11]
-	/// [e12,e13,e14,e15]
-	/// \endcode 
-	/// 
-	/// Left-hand system
-	/// ========================
-	/// - Z-Axis positive direction points into screen
-	/// - Direction of cross product of two vectors follows \n 
-	/// left hand rule(clock-wise)
-	///
-	/// Left-multiply system
-	/// ========================
-	/// - Vector left multiply Matrix \n 
-	/// \code 
-	/// v[x,y,z,w]*[m00,m01,m02,m03]
-	///            [m10,m11,m12,m13]
-	///            [m20,m21,m22,m23]
-	///            [m30,m31,m32,m33]
-	/// \endcode
-	///
-	/// Matrix/Projection conversion
-	/// ========================
-	/// - Z value range [0,1]
-	/// - Z value direction [near, far]
-	///
-	/// Detailed Doc of Math Heading 1
-	/// ========================
-	/// Something after heading
-	/// with function reference \ref Abs and \ref IsNAN
+    /// Brief contine
+    ///
+    /// This implementation of Math/Engine libraries basically follows
+    /// conventions of DirectX/HLSL/Windows.
+    ///
+    /// Row major system
+    /// ========================
+    /// - Vector is row major [x,y,z,w]
+    /// - Matrix is row major \n
+    /// \code
+    /// [e0, e1, e2, e3 ]
+    /// [e4, e5, e6, e7 ]
+    /// [e8, e9, e10,e11]
+    /// [e12,e13,e14,e15]
+    /// \endcode
+    ///
+    /// Left-hand system
+    /// ========================
+    /// - Z-Axis positive direction points into screen
+    /// - Direction of cross product of two vectors follows
+    /// left hand rule(clock-wise)
+    ///
+    /// Left-multiply system
+    /// ========================
+    /// - Vector left multiply Matrix \n
+    /// \code
+    /// v[x,y,z,w]*[m00,m01,m02,m03]
+    ///            [m10,m11,m12,m13]
+    ///            [m20,m21,m22,m23]
+    ///            [m30,m31,m32,m33]
+    /// \endcode
+    ///
+    /// Matrix/Projection conversion
+    /// ========================
+    /// - Z value range [0,1]
+    /// - Z value direction [near, far]
+    ///
+    /// Detailed Doc of Math Heading 1
+    /// ========================
+    /// Something after heading
+    /// with function reference \ref Abs and \ref IsNAN
     namespace Math
     {
         /// \brief PI constant
@@ -147,7 +147,7 @@ namespace vEngine
 
         template <typename T, int N = 4>
         Matrix<T, N, N> OuterProduct(Vector<T, N> const& lhs,
-                                       Vector<T, N> const& rhs);
+                                     Vector<T, N> const& rhs);
 
         template <typename T, int I = 4, int J = 4, int K = 4>
         Matrix<T, I, K> Multiply(const Matrix<T, I, J>& lhs,
@@ -156,7 +156,7 @@ namespace vEngine
     }  // namespace Math
 }  // namespace vEngine
 
-//include template definitions after declearations above
-//for separate declearation/definition of template functions
+// include template definitions after declearations above
+// for separate declearation/definition of template functions
 #include <vengine/core/math.hpp>
 #endif /* _MATH_H */
