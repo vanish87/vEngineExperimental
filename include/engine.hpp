6efd51c7e859147dfd35c5f99ca7fc8c56ee0a2e
Yuan Li <vanish8.7@gmail.com>
2021-12-24 20:05:58 +0900

    add define guild for configure files

--- include/engine.hpp
+++ include/engine.hpp
@@ -2,7 +2,7 @@
 // Note math.h will include math.hpp
 #include <vengine/core/math.h>
 
-#include <compiler.hpp>
+#include <compiler_setting.hpp>
 #include <version.hpp>
 #include <vengine/core/application.hpp>
 #include <vengine/core/dll.hpp>
