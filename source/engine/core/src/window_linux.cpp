22dfdf3b3d7bdde87ac7e3dde1b8167051f53368
Yuan Li <vanish8.7@gmail.com>
2021-12-27 18:39:39 +0900

    add dummy implement in linux

--- source/engine/core/src/window_linux.cpp
+++ source/engine/core/src/window_linux.cpp
@@ -1,5 +1,22 @@
 
 #ifdef VENGINE_PLATFORM_LINUX
-#include <vengine/core/window.hpp>
+    #include <vengine/core/window.hpp>
+
+namespace vEngine
+{
+    namespace Core
+    {
+        struct WindowRect
+        {
+                int top;
+                int left;
+                int width;
+                int height;
+        };
+        void Window::Init(...) {}
+        void Window::Deinit(...) {}
+        void Window::Update() {}
+    }  // namespace Core
+}  // namespace vEngine
 
 #endif
