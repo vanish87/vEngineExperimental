741702f1c73a0a111fd5d3b7c3fdbf43b09bf319
Yuan Li <vanish8.7@gmail.com>
2021-12-27 13:29:51 +0900

    rename window file

---
+++ source/engine/core/src/window_win32.cpp
@@ -0,0 +1,25 @@
+
+#ifdef WINDOWS
+
+#include <vengine/core/window.hpp>
+#include <vengine/core/debug.hpp>
+
+namespace vEngine
+{
+    namespace Core
+    {
+        void Window::Init(...)
+        {
+
+        }
+        void Window::Deinit(...)
+        {
+
+        }
+        void Window::Update()
+        {
+        }
+    }  // namespace Core
+}  // namespace vEngine
+
+#endif
