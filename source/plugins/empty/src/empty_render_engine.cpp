cbd5037a95e4138c7e9e43f307bc5e3e7d51ca4b
Yuan Li <vanish8.7@gmail.com>
2022-01-19 12:48:02 +0900

    add empty dll

---
+++ source/plugins/empty/src/empty_render_engine.cpp
@@ -0,0 +1,37 @@
+
+#include <vengine/rendering/empty_render_engine.hpp>
+
+namespace vEngine
+{
+    namespace Rendering
+    {
+
+        void EmptyRenderEngine::TestFunc(){
+
+        };
+
+        void EmptyRenderEngine::CreateRenderWindow(void* hwnd)
+        {
+            UNUSED_PARAMETER(hwnd);
+        };
+        void EmptyRenderEngine::Render(){
+
+        };
+        void EmptyRenderEngine::PrintInfo()
+        {
+            std::cout << "Empty" << std::endl;
+        };
+
+    }  // namespace Rendering
+}  // namespace vEngine
+
+extern "C" {
+    void CreateRenderEngine(std::unique_ptr<vEngine::Rendering::RenderEngine>& ptr)
+    {
+        ptr = std::make_unique<vEngine::Rendering::EmptyRenderEngine>();
+    }
+    void DestoryRenderEngine(std::unique_ptr<vEngine::Rendering::RenderEngine>& ptr)
+    {
+        ptr.reset();
+    }
+}
