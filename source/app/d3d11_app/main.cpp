9627f1c762c4722b3281afd652eea314604cfe88
Yuan Li <vanish8.7@gmail.com>
2022-01-20 17:20:21 +0900

    refine example app code

---
+++ source/app/d3d11_app/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+// #include <windows.h>
+#include <engine.hpp>
+// #include <vengine/core/example_class_header.h>
+#include <vengine/core/application.hpp>
+#include <vengine/core/constants.hpp>
+#include <vengine/core/context.hpp>
+#include <version.hpp>
+
+using namespace vEngine::Core;
+
+class MyApp : public Application
+{
+    private:
+        void OnCreate() override
+        {
+            std::cout << "User Create" << std::endl;
+        }
+        void OnUpdate() override
+        {
+            // std::cout << "User Update" << vEngine::TIME_PER_UPDATE << std::endl;
+        }
+};
+
+int main(int argc, char* argv[])
+{
+    UNUSED_PARAMETER(argc);
+    UNUSED_PARAMETER(argv);
+
+    std::cout << "Version " + std::string(Version) << std::endl;
+
+    Configure configure;
+    configure.app_name = "D3D11 Example";
+    configure.graphics_configure.width = 1280;
+    configure.graphics_configure.height = 720;
+    configure.graphics_configure.render_plugin_name = "d3d11_rendering_plugin";
+
+    Context::GetInstance().ConfigureWith(configure);
+
+    MyApp myapp;
+    myapp.Init();
+    myapp.Run();
+}
