c444a41e74d54e621d3ade221d4da99dd07878e3
vanish87 <vanish8.7@gmail.com>
2022-03-06 10:33:43 +0900

    add mesh renderer component

---
+++ source/engine/core/mesh_renderer_component.cpp
@@ -0,0 +1,24 @@
+/// \file mesh_renderer_component.cpp
+/// \brief source file for Mesh Renderer Component
+///
+/// A detailed file description.
+///
+/// \author author_name
+/// \version version_number
+/// \date xxxx-xx-xxx
+
+#include <vengine/core/mesh_renderer_component.hpp>
+
+/// A detailed namespace description, it
+/// should be 2 lines at least.
+namespace vEngine
+{
+    namespace Core
+    {
+
+        /// constructor detailed defintion,
+        /// should be 2 lines
+        MeshRendererComponent::MeshRendererComponent() {}
+    }  // namespace Core
+
+}  // namespace vEngine
