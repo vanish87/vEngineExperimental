61e9a6a84e3b31261ecc6ab6513dfe10d93f1858
Yuan Li <vanish8.7@gmail.com>
2022-07-29 20:39:11 +0900

    use resoruce folder to build resoruce

--- source/pipeline/smpl_to_json/main.cpp
+++ source/pipeline/smpl_to_json/main.cpp
@@ -1,9 +1,14 @@
 
 #include <engine.hpp>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
     UNUSED_PARAMETER(argc);
     UNUSED_PARAMETER(argv);
+    std::ofstream outfile("my.json");
+    outfile<<"MyJson"<<std::endl;
+    outfile.flush();
+    outfile.close();
     return 0;
 }
