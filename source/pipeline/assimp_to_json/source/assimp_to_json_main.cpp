787d238e2881f16600a33a1500407cfd87801fbb
vanish87 <vanish8.7@gmail.com>
2023-06-09 09:26:58 +0900

    update debug prefix

--- source/pipeline/assimp_to_json/source/assimp_to_json_main.cpp
+++ source/pipeline/assimp_to_json/source/assimp_to_json_main.cpp
@@ -27,10 +27,12 @@ int main(int argc, char* argv[])
         if (s == "-r") resource_src = argv[i + 1];
         if (s == "-b") resource_bin = argv[i + 1];
     }
-    // input = "C:/Users/liyuan/Documents/Personal/vEngineExperimental/resource/boblamp/boblampclean.md5mesh";
-    // output = "C:/Users/liyuan/Documents/Personal/vEngineExperimental/build_windows/resource/bin/assimp/boblampclean.json";
-    // resource_src = "C:/Users/liyuan/Documents/Personal/vEngineExperimental/resource";
-    // resource_bin = "C:/Users/liyuan/Documents/Personal/vEngineExperimental/build_windows/resource/bin";
+    // std::string prefix = "C:/Users/vanis/Documents/Code/vEngineExperimental";
+    // std::string prefix = "C:/Users/liyuan/Documents/Personal/vEngineExperimental";
+    // input = prefix + "/resource/boblamp/boblampclean.md5mesh";
+    // output = prefix + "/resource/assimp/boblampclean.json";
+    // resource_src = prefix + "/resource";
+    // resource_bin = prefix + "/build_windows/resource/bin";
 
     Configure configure;
     configure.app_name = "assimp_to_json";
