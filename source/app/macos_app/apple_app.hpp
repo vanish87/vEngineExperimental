716cdd13ca250e07c19a2e91f2585569071570db
vanish87 <vanish8.7@gmail.com>
2023-07-21 22:11:37 +0900

    rollback to old code for view/application

---
+++ source/app/macos_app/apple_app.hpp
@@ -0,0 +1,36 @@
+#ifndef _APP_MACOS_APP_APPLE_APP_HPP
+#define _APP_MACOS_APP_APPLE_APP_HPP
+
+#include <engine.hpp>
+#include <vengine/core/application.hpp>
+#include <version.hpp>
+
+using namespace vEngine::Core;
+
+/// Namespace for example applications
+namespace AppleApp
+{
+    /// \brief A general example of user application
+    class ExampleApp : public Application
+    {
+        private:
+            /// \brief user function for create
+            ///
+            void OnInit() override
+            {
+                PRINT("User Create");
+            }
+            /// \brief user function for update
+            ///
+            /// Will be call at const seconds
+            /// vEngine::TIME_PER_UPDATE
+            void OnUpdate() override {}
+            /// \brief user function for destroy
+            void OnDeinit() override
+            {
+                PRINT("User Destory");
+            }
+    };
+}  // namespace AppleApp
+
+#endif /* _APP_MACOS_APP_APPLE_APP_HPP */
