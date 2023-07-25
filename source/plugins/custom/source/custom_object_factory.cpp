b5c374603d76d9754df6a4a7e2f3808b803614d4
Li Yuan <liyuan@team-lab.com>
2023-07-25 12:46:11 +0900

    fix linux build error

--- source/plugins/custom/source/custom_object_factory.cpp
+++ source/plugins/custom/source/custom_object_factory.cpp
@@ -7,8 +7,8 @@ namespace vEngine
     namespace Custom
     {
         using namespace vEngine::Core;
-        CustomObjectFactory::CustomObjectFactory(){};
-        CustomObjectFactory::~CustomObjectFactory(){};
+        CustomObjectFactory::CustomObjectFactory(){}
+        CustomObjectFactory::~CustomObjectFactory(){}
         GameObjectSharedPtr CustomObjectFactory::Create(std::any parameter)
         {
             UNUSED_PARAMETER(parameter);
